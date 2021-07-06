49950926407eb0bde44869b9604327a6e8f4d204
Tomás Garcia <tomy.garcia@hotmail.com>
2021-07-06 18:48:31 -0300

    example, working

--- index.js
+++ index.js
@@ -2,9 +2,38 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Connected
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+// Disconnected
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+
+// Print received message
+// io.on('connection', (socket) => {
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//   });
+// });
+
+// send the message to everyone
+io.on('connection', (socket) => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () => {
